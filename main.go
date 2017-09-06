9bb4da41dbba602531dacbbb7ea5f2ca740b1c34
Yaroslav Molochko <onorua@gmail.com>
2017-09-07 01:01:11 +0300

    Introduce logging (#5)

--- main.go
+++ main.go
@@ -1,48 +1,75 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/goware/urlx"
 
+	"github.com/Sirupsen/logrus"
 	"github.com/anchorfree/prometheus-ha-proxy/merger"
 )
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	logrus.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	logrus.SetLevel(logrus.InfoLevel)
+}
 
 type BackendOutput struct {
 	Body []byte
 	Err  error
 }
 
-func main() {
+type PromData struct {
+	Logger *logrus.Entry
+	URLs   []*url.URL
+}
 
-	http.HandleFunc("/", PrometheusProxy)    // set router
-	err := http.ListenAndServe(":9090", nil) // set listen port
+func main() {
+	addresses := os.Args[1:]
+	logger := logrus.WithFields(logrus.Fields{
+		"app": "prometheus-ha-proxy",
+	})
+
+	var data PromData
+	data.Logger = logger
+	for _, a := range addresses {
+		url, err := urlx.Parse(a)
+		if err != nil {
+			// we could not do parse address url
+			// TODO: logging
+			logger.Warn("could not parse address due to", err)
+			continue
+		}
+		data.URLs = append(data.URLs, url)
+	}
+
+	http.HandleFunc("/", data.PrometheusProxy) // set router
+	err := http.ListenAndServe(":9090", nil)   // set listen port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func PrometheusProxy(w http.ResponseWriter, r *http.Request) {
-	addresses := os.Args[1:]
-	// TODO: logging
-	fmt.Println(r.URL.String())
+func (p *PromData) PrometheusProxy(w http.ResponseWriter, r *http.Request) {
+	logger := p.Logger
+	logger.Info(r.URL.String())
 	var buffers []*[]byte
 	ch := make(chan BackendOutput)
 
 	if r.Method == "GET" {
-		for _, a := range addresses {
-			base, err := urlx.Parse(a)
-			if err != nil {
-				// we could not do parse address url
-				// TODO: logging
-				fmt.Println(err)
-				continue
-			}
-			go PromGet(base.ResolveReference(r.URL).String(), ch)
+		for _, a := range p.URLs {
+			go PromGet(logger, a.ResolveReference(r.URL).String(), ch)
 		}
 
 		var cnt int
@@ -53,7 +80,7 @@ func PrometheusProxy(w http.ResponseWriter, r *http.Request) {
 			}
 			buffer := out.Body
 			buffers = append(buffers, &buffer)
-			if cnt == len(addresses) {
+			if cnt == len(p.URLs) {
 				close(ch)
 			}
 		}
@@ -64,13 +91,12 @@ func PrometheusProxy(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func PromGet(url string, ch chan BackendOutput) {
+func PromGet(logger *logrus.Entry, url string, ch chan BackendOutput) {
 	var out BackendOutput
 
 	res, err := http.Get(url)
 	if err != nil {
-		// TODO: logging
-		fmt.Println("could not query due to: ", err)
+		logger.Warning("could not query ", url, "due to: ", err)
 		out.Err = err
 		ch <- out
 		return
@@ -81,8 +107,7 @@ func PromGet(url string, ch chan BackendOutput) {
 		out.Body, out.Err = ioutil.ReadAll(res.Body)
 		ch <- out
 	} else {
-		// Error based on status code received
-		fmt.Println("Did not work out due to: ", res.StatusCode)
+		logger.Warning("Result code is not success: ", res.StatusCode)
 	}
 	return
 }
